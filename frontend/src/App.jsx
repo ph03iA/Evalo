a0cbfd78fd11e90eaf322c3a18e39ae7837ebfaf
Prajwal Chahande <134228742+ph03iA@users.noreply.github.com>
2025-12-05 02:14:56 +0530

    Fix DB connection and Clerk imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { SignInButton, SignedOut, SignedIn, SignOutButton, UserButton } from '@clerk/clerk-react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to Evalo</h1>
+
+      <SignedOut>
+        <SignInButton mode="modal" >
+          <button className="">Sign In</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <UserButton />
     </>
-  )
+  );
 }
 
 export default App
