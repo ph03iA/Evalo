1c1ee644e92e4bfee1bc3a2db6cf79cd7cd125fd
Prajwal Chahande <134228742+ph03iA@users.noreply.github.com>
2025-12-13 04:45:16 +0530

    landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import { useUser } from '@clerk/clerk-react'
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import { Toaster } from 'react-hot-toast';
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+  //this will get rid of the flickering effect when we reload dashboard page
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}></Route>
       </Routes>
       
@@ -20,5 +24,3 @@ function App() {
 }
 
 export default App
-// tw, diasy ui, react-router, react-hot-toast,
-// todo: react query aka tanstack query. axios
